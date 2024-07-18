3ca8d9dfdcb7825eda4c1fbc316da8c334705d93
rahahahaa <rahulbhat7169@gmail.com>
2024-07-18 12:30:46 +0530

    Youtube-clone

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "f5fac844f9mshfc0fd216f97d61cp1b0bdcjsnba58d3b7cacc",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
